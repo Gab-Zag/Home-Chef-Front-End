c89ab8438c0f6e63b3281c00349baade907c825b
Gabriel Alves Pereira <145914573+Gab-Zag@users.noreply.github.com>
2025-12-01 18:17:19 -0300

    Update base URL in ApiService to correct endpoint for recipe retrieval

--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -5,7 +5,7 @@ import '../models/recipe.dart';
 import 'i_recipe_api.dart';
 
 class ApiService implements IRecipeApi {
-  static const String baseUrl = 'https://mozell-nonexaggerative-conjugably.ngrok-free.dev/recipes/all';
+  static const String baseUrl = 'https://mozell-nonexaggerative-conjugably.ngrok-free.dev/recipes';
 
   @override
   Future<List<Recipe>> searchRecipes(String ingredients) async {
